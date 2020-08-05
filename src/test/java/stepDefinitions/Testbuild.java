7a1c68c06802c19cf90e8d9700e533b37053964f
Santhosh KT <ktsanthosh08@gmail.com>
2020-08-05 20:01:08 +0530

    modified

--- src/test/java/stepDefinitions/Testbuild.java
+++ src/test/java/stepDefinitions/Testbuild.java
@@ -3,16 +3,11 @@ package stepDefinitions;
 public class Testbuild {
 	
 	public static void main(String[] args) {
-		System.out.println("hello");
-		
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
-		System.out.println("GitStuff for practicing");
+		System.out.println("REvision");
+		System.out.println("REvision");
+		System.out.println("REvision");
+		System.out.println("REvision");
+		System.out.println("REvision");
 	}
 
 }
